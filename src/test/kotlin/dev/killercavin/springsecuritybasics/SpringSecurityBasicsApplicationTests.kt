aed79d230263439c9fb29173418de1b084bc31f9
Cavin <cavinlarry001@gmail.com>
2025-09-14 23:29:22 +0300

    Initial setup -> spring security basics

---
+++ src/test/kotlin/dev/killercavin/springsecuritybasics/SpringSecurityBasicsApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.killercavin.springsecuritybasics
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSecurityBasicsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
